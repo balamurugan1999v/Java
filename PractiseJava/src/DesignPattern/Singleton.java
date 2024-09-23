13720912cc4fbec6315f746fbf8d07732ad51379
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2024-09-24 00:41:00 +0530

    Serializable and DeSerializable

--- PractiseJava/src/DesignPattern/Singleton.java
+++ PractiseJava/src/DesignPattern/Singleton.java
@@ -1,8 +1,16 @@
 package DesignPattern;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 public class Singleton {
 	
-	public static class SingletonExample{
+	public static class SingletonExample implements Serializable{
 		
 		private  int sum =0;
 		public static SingletonExample classobject;
@@ -13,8 +21,11 @@ public class Singleton {
 			if( classobject == null) {
 				classobject = new SingletonExample();								
 			}
+			return classobject;			
+		}
+		
+		public Object readResolve() { // It is used to create a same object when we are using deserialization
 			return classobject;
-			
 		}
 		
 		public void setSum(int sum) {
@@ -27,9 +38,18 @@ public class Singleton {
 	}
 	
 	public class main{
-		public static void main(String args[]) {
+		public static void main(String args[]) throws IOException, ClassNotFoundException {
 			SingletonExample obj = SingletonExample.getInstance();
-			SingletonExample obj1 = SingletonExample.getInstance();
+			SingletonExample obj1 ;
+			FileOutputStream fos = new FileOutputStream(new File("C:\\Users\\ELCOT\\Desktop\\FullStack\\Java\\PractiseJava\\src\\DesignPattern\\Singleton\\file.ser"));
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(obj);
+			FileInputStream fis = new FileInputStream("C:\\Users\\ELCOT\\Desktop\\FullStack\\Java\\PractiseJava\\src\\DesignPattern\\Singleton\\file.ser");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			obj1 = (SingletonExample) ois.readObject();
+			if(obj1 == obj) {
+				System.out.println("True");
+			}
 			obj1.setSum(5);
 			obj.print();
 		}
