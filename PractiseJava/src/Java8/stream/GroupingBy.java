df999f6af675032a0e3ce3340b560c7813d09e78
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2025-06-14 15:38:47 +0530

    GroupingBy in stream api

---
+++ PractiseJava/src/Java8/stream/GroupingBy.java
@@ -0,0 +1,31 @@
+package Java8.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GroupingBy {
+
+	public static void main(String[] args) {
+		String s = "abcacbbb";
+		char[] ch = s.toCharArray();
+		Map<Character, Long> freq = s.chars()
+			    .mapToObj(c -> (char) c)
+			    .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+		//Map<Character,Integer> map = Arrays.stream(ch)
+			//	.collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+		
+		System.out.println(freq);
+		
+		Employee emp1 = new Employee("Priya", "Dev", "16");
+		Employee emp2 = new Employee("Kumar", "Verification", "24");
+		Employee emp3 = new Employee("Adam", "Dev", "14");
+		List<Employee> list = new ArrayList<>(List.of(emp1,emp2,emp3));
+		
+		Map<String, Long> empDetails = list.stream().collect(Collectors.groupingBy( Employee::getRole,Collectors.counting()));
+		
+		System.out.println(empDetails);
+	}
+
+}
